0025587f3850b04cb805eea53a49d2cdd297a133
rhishikesh <rhishikesh@helpshift.com>
2021-03-09 21:45:14 +0530

    Add lin-kv dependency to make the datomic implementation a shared state.

--- demo/clojure/datomic.clj
+++ demo/clojure/datomic.clj
@@ -11,6 +11,12 @@
 ;;;;;; Input pre-processing functions ;;;;;;
 
 
+(defn- read-stdin
+  "Read a single line from stdin and return it"
+  []
+  (read-line))
+
+
 (defn- process-stdin
   "Read lines from the stdin and calls the handler"
   [handler]
@@ -57,7 +63,6 @@
 (def node-id (atom ""))
 (def node-nbrs (atom []))
 (def next-message-id (atom 0))
-(def state (atom {}))
 
 
 (defn- reply
@@ -76,17 +81,16 @@
    (send! (reply src dest body))))
 
 
-(defn- replicate-loop
-  []
-  (future
-    (try
-      (loop []
-        (doseq [n @node-nbrs]
-          (send! (reply @node-id n {:type "replicate"})))
-        (Thread/sleep 5000)
-        (recur))
-      (catch Exception e
-        (printerr e)))))
+(defn- call-service
+  [service body]
+  (send! @node-id service body)
+  ;; @WARN : This code assumes that when we
+  ;; send a message to lin-kv (write to stdout)
+  ;; the next line we get on stdin will be its reply
+  ;; How does maelstrom do this ?? What if there are pending
+  ;; inputs on stdin which we haven't processed ?
+  (let [reply (read-stdin)]
+    (parse-json reply)))
 
 
 (defn- process-txns
@@ -94,12 +98,27 @@
   (mapv (fn [[f k v :as txn]]
           (case f
             "r"
-            ["r" k (get @state k)]
+            ["r" k (->> {:type "read"
+                         :key k}
+                        (call-service "lin-kv")
+                        :body
+                        :value)]
 
             "append"
-            (do
-              (swap! state update k (fnil conj []) v)
-              txn)))
+            (let [from (->> {:type "read"
+                             :key k}
+                            (call-service "lin-kv")
+                            :body
+                            :value)
+                  reply (->> {:type "cas"
+                              :key k
+                              :from from
+                              :to ((fnil conj []) from v)
+                              :create_if_not_exists true}
+                             (call-service "lin-kv")
+                             :body)]
+              (when (= "cas_ok" (:type reply))
+                txn))))
         txns))
 
 
@@ -134,7 +153,6 @@
 (defn -main
   "Read transactions from stdin and send output to stdout"
   []
-  ;;(replicate-loop)
   (process-stdin (comp printout
                        generate-json
                        process-request
